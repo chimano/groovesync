4098628ca2c06a8c0c3579b74b33b71844e6d5f2
Alvyn Duy-Khoi Le <alvyn279@gmail.com>
2019-01-26 18:48:50 -0500

    get artist and song names (not fully implemented)

--- README.md
+++ README.md
@@ -5,9 +5,24 @@ client_secret = '<TouchTunes API-KEY>'
 ```
 
 3 environment variables are needed for the database connection
+Add these to `activate` script in your virtual environment
 
 ```dosini
 DB_USERNAME="<username of the database user>"
 DB_PASSWORD="<password of the database user>"
 DB_NAME="<name of the database>"
+```
+
+Migrations:
+```
+# To run migrations
+alembic upgrade head
+
+# To create a migration
+alembic revision -m '<name>'
+```
+
+To run Flask server:
+```shell
+python app.py
 ```
--- backend/services/main.py
+++ backend/services/main.py
@@ -1,12 +1,45 @@
 import requests
 import json
+import mysql.connector
 
 from settings import client_secret
 
+BASE_URL = 'https://conuhacks-playback-api.touchtunes.com/'
+REQUEST_HEADERS = {
+    'client-secret': client_secret
+}
+
+def getArtist(id):
+    URL = BASE_URL + 'artist/%s' % (id)
+    r = requests.get(URL, headers=REQUEST_HEADERS)
+    return r
+
+def getSong(id):
+    URL + BASE_URL + 'song/%s' % (id)
+    r = requests.get(URL, headers=REQUEST_HEADERS)
+    return r
+
 if __name__ == '__main__':
-    URL =  'https://conuhacks-playback-api.touchtunes.com/plays?startDate=2018-02-19T21:00:00Z&endDate=2018-02-19T22:00:00Z&offset=0'
-    headers = {
-        'client-secret': client_secret
-    }
-    r = requests.get(URL,headers=headers).json()
-    print(r)
+    URL = BASE_URL + 'plays?startDate=2018-02-19T21:00:00Z&endDate=2018-02-19T22:00:00Z&offset=0'
+    
+    r = requests.get(URL,headers=REQUEST_HEADERS)
+
+    location_songs = {}
+    # organize locations as keys tuple and dictionary of songs
+    # for every location
+    for p in r.json()['plays']:
+        location = (p['latitude'], p['longitude'])
+        if location in location_songs:
+            location_songs[location].append(p)
+        else:
+            location_songs[location] = [p]
+
+    # add song and artist names to un-flattened dict
+    for l in location_songs:
+        for s in l:
+            artist = getArtist(s['artistId'])
+            song = getSong(s['songId'])
+            s['songName'] = song
+            s['artistName'] = artist
+
+    print(location_songs)
