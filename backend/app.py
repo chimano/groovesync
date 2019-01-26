bbade78afc25a1e0d50cbce2a7c72d10c4e61d04
Chirac <chiracmanoukian@hotmail.com>
2019-01-26 16:15:36 -0500

    DB config almost done

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,23 @@
 from flask import Flask
+from models import Testtable
+from db import session
+from serializer import JSONSerializer
+import json
+import pdb
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+
+    my_test = Testtable("wew")
+    session.add(my_test)
+    res = session.query(Testtable).all()
+    for r in res:
+        print(json.dumps(r, cls=JSONSerializer))
+
+    # return res
+
 
 if __name__ == '__main__':
     app.run()
