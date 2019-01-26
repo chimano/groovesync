30c497cf90a05f67501e272f62e743c67b9fa3a5
Lucas Turpin <lucas_turpin@hotmail.com>
2019-01-26 17:17:44 -0500

    Convert app to kotlin

---
+++ groovesync/app/src/main/java/tech/groovesync/groovesync/MainActivity.kt
@@ -0,0 +1,17 @@
+package tech.groovesync.groovesync
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        findViewById<TextView>(R.id.hello).setText("Hello World!")
+    }
+}
