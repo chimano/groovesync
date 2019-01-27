ebf6c884ef43b5707987ac8771e123e244a71784
panoreak <panoreak@users.noreply.github.com>
2019-01-26 20:46:37 -0500

    Added map to main activity

--- groovesync/app/src/main/java/tech/groovesync/groovesync/MainActivity.kt
+++ groovesync/app/src/main/java/tech/groovesync/groovesync/MainActivity.kt
@@ -1,17 +1,30 @@
 package tech.groovesync.groovesync
 
-import android.app.Activity
 import android.os.Bundle
-import android.widget.TextView
+import android.support.v4.app.FragmentActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 
-class MainActivity : Activity() {
+class MainActivity : FragmentActivity(), OnMapReadyCallback {
+
+    private var mMap: GoogleMap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
     }
 
-    override fun onStart() {
-        super.onStart()
-        findViewById<TextView>(R.id.hello).setText("Hello World!")
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val concordia = LatLng(45.5554472,-73.7068921)
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(concordia))
+        mMap!!.setMinZoomPreference(10f)
     }
 }
