c7f1b983c6e86996a336b79b4eac1ddb396419ed
Chirac <chiracmanoukian@hotmail.com>
2019-01-27 03:29:46 -0500

    Made the spotify connector into a module

--- backend/connectors/SpotifyConnector.py
+++ backend/connectors/SpotifyConnector.py
@@ -2,61 +2,81 @@ import requests
 import base64
 import datetime
 import json
+import os
 
 
-class SpotifyConnector:
-    def __init__(self, client_id, secret_id, user_token=None):
+client_id = os.environ['SPOTIFY_CLIENT_ID']
+secret_id = os.environ['SPOTIFY_SECRET_ID']
+AUTH_URL = 'https://accounts.spotify.com/'
+API_URL = 'https://api.spotify.com/'
+access_token = refresh_token()
+acquired_at = datetime.datetime.now()
+headers = {
+    'Content-Type': "application/x-www-form-urlencoded",
+    'Authorization': "Bearer %s" % (access_token)
+}
+auth_headers = {
+    'Content-Type': "application/x-www-form-urlencoded",
+    'Authorization': base64.b64encode('%s:%s' % (client_id, secret_id))
+}
 
-        self.AUTH_URL = 'https://accounts.spotify.com/'
-        self.API_URL = 'https://api.spotify.com/'
-        self.access_token = self.refresh_token()
-        self.acquired_at = datetime.datetime.now()
-        self.headers = {
+MAX_LIMIT = 20
+
+
+def refresh_token():
+
+    r = requests.post(AUTH_URL + 'api/token', headers=auth_headers,
+                      data={'grant_type': 'client_credentials'})
+    return r['access_token']
+    auth_headers = {
+        'Content-Type': "application/x-www-form-urlencoded",
+        'Authorization': base64.b64encode('%s:%s' % (client_id, secret_id))
+    }
+
+
+def search_song(song_name, artist_name):
+    r = requests.get(API_URL + "v1/search",
+                     params={"q": "name:" + song_name,
+                               "type": "track"},
+                     headers=headers)
+    if r.status_code == 401:
+        access_token = refresh_token()
+        global headers = {
             'Content-Type': "application/x-www-form-urlencoded",
-            'Authorization': "Bearer %s" % (self.access_token)}
-
-        self.MAX_LIMIT = 20
-
-    def refresh_token(self):
-        auth_headers = {
-            'Content-Type': "application/x-www-form-urlencoded",
-            'Authorization': base64.b64encode('%s:%s' % (client_id, secret_id))}
+            'Authorization': "Bearer %s" % (access_token)
         }
+        return search_song(song_name, artist_name)
 
-        r = requests.post(self.AUTH_URL + 'api/token', headers=auth_headers,
-                          data = {'grant_type': 'client_credentials'})
-        return r['access_token']
+    response_dict = json.loads(r.text)
+    for track in response_dict:
+        for artist in track['artists']:
+            if artist['name'].lower() == artist_name.lower():
+                return track['id']
 
-
-    def search_song(self, song_name, artist_name):
-        r=requests.get(self.API_URL + "v1/search",
-                    params={"q":"name:" + song_name,
-                            "type": "track"},
-                    headers=self.headers)
-        
-        response_dict = json.loads(r.text)
-        for track in response_dict:
-            for artist in track['artists']:
-                if artist['name'].lower() == artist_name.lower():
-                    return track['id']
-        
-        return -1
+    return -1
 
 
-    def get_song_features(self, ids):
-        features = []
-        for i in range(0, len(ids), self.MAX_LIMIT):
-            batched_ids = ids[i:i+self.MAX_LIMIT]
-            try:
+def get_song_features(ids, user_token=None):
+    features = []
+    for i in range(0, len(ids), MAX_LIMIT):
+        batched_ids = ids[i:i+MAX_LIMIT]
+        try:
+            r = requests.get(API_URL + "v1/audio-features",
+                             params={'ids': ','.join(batched_ids)},
+                             headers=headers)
 
-                r = requests.get(self.API_URL + "v1/audio-features", 
-                            params={'ids':','.join(batched_ids)},
-                            headers=self.headers)
-                        
-                response_dict = json.loads(r.text)
-                features += response_dict['audio_features']
-            except:
-                print("Something went wrong with the features")
-                return -1
+            response_dict = json.loads(r.text)
+            features += response_dict['audio_features']
+            if r.status_code == 401:
+                global headers = {
+                    'Content-Type': "application/x-www-form-urlencoded",
+                    'Authorization': "Bearer %s" % (access_token)
+                }
+                access_token = refresh_token()
+                return get_song_features(song_name, artist_name)
+        except:
+            print("Something went wrong with the features")
+            break
+            return -1
 
-        return features
+    return features
