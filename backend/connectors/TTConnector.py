07ff11dc461a13bb56f4739aa2c3b0ab40ecafde
Chirac <chiracmanoukian@hotmail.com>
2019-01-27 04:36:43 -0500

    Made TTConnector a module instead of class

--- backend/connectors/TTConnector.py
+++ backend/connectors/TTConnector.py
@@ -1,19 +1,20 @@
 import requests
+import os
+
+api_key = os.environ['TT_API_KEY']
+headers = {
+    'client-secret': api_key
+}
+BASE_URL = 'https://conuhacks-playback-api.touchtunes.com/'
 
 
-class TTConnector:
-    def __init__(self, api_key):
-        self.headers = {
-            'client-secret': api_key
-        }
-        self.BASE_URL = 'https://conuhacks-playback-api.touchtunes.com/'
+def get_artist_name(artist_id):
+    r = requests.get(BASE_URL + 'artist/%s' %
+                     (artist_id), headers=headers)
+    return r.json()['artistTitle']
 
-    def get_artist_name(self, artist_id):
-        r = requests.get(self.BASE_URL + 'artist/%s' %
-                         (artist_id), headers=self.headers)
-        return r.json()['artistTitle']
 
-    def get_song_name(self, tunes_id):
-        r = requests.get(self.BASE_URL + 'song/%s' %
-                         (tunes_id), headers=self.headers)
-        return r.json()['songTitle']
+def get_song_name(tunes_id):
+    r = requests.get(BASE_URL + 'song/%s' %
+                     (tunes_id), headers=headers)
+    return r.json()['songTitle']
--- backend/services/TTinfo.py
+++ backend/services/TTinfo.py
@@ -1,4 +1,4 @@
-from connectors.TTConnector import TTConnector
+from connectors.TTConnector import get_song_name, get_artist_name
 from db import session
 from models import Songs, Artists
 api_key = "9923ac9b-8fd3-421f-b0e5-952f807c6885"
@@ -10,7 +10,7 @@ def main():
     songs = session.query(Songs).all()
 
     for song in songs:
-        song_name = tt_connector.get_song_name(song.tunes_id)
+        song_name = get_song_name(song.tunes_id)
         song.name = song_name
 
     session.commit()
@@ -18,7 +18,7 @@ def main():
     artists = session.query(Artists).all()
 
     for artist in artists:
-        artist_name = tt_connector.get_artist_name(artist.id)
+        artist_name = get_artist_name(artist.id)
         artist.name = artist_name
 
     session.commit()
