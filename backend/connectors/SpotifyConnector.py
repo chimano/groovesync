680e561afb53932ba889d62b0d0195b6135a8ab5
Chirac <chiracmanoukian@hotmail.com>
2019-01-27 02:52:19 -0500

    Completed spotify connector

--- backend/connectors/SpotifyConnector.py
+++ backend/connectors/SpotifyConnector.py
@@ -15,6 +15,8 @@ class SpotifyConnector:
             'Content-Type': "application/x-www-form-urlencoded",
             'Authorization': "Bearer %s" % (self.access_token)}
 
+        self.MAX_LIMIT = 20
+
     def refresh_token(self):
         auth_headers = {
             'Content-Type': "application/x-www-form-urlencoded",
@@ -25,10 +27,36 @@ class SpotifyConnector:
                           data = {'grant_type': 'client_credentials'})
         return r['access_token']
 
+
     def search_song(self, song_name, artist_name):
-        formatted_name=song_name.replace(' ', '%20')
-        r=requests.get(self.API_URL + "v1/search?name:%s&type=track" % (formatted_name),
-                       headers=self.headers)
+        r=requests.get(self.API_URL + "v1/search",
+                    params={"q":"name:" + song_name,
+                            "type": "track"},
+                    headers=self.headers)
         
         response_dict = json.loads(r.text)
-        for track in 
+        for track in response_dict:
+            for artist in track['artists']:
+                if artist['name'].lower() == artist_name.lower():
+                    return track['id']
+        
+        return -1
+
+
+    def get_song_features(self, ids):
+        features = []
+        for i in range(0, len(ids), self.MAX_LIMIT):
+            batched_ids = ids[i:i+self.MAX_LIMIT]
+            try:
+
+                r = requests.get(self.API_URL + "v1/audio-features", 
+                            params={'ids':','.join(batched_ids)},
+                            headers=self.headers)
+                        
+                response_dict = json.loads(r.text)
+                features += response_dict['audio_features']
+            except:
+                print("Something went wrong with the features")
+                return -1
+
+        return features
