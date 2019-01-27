6bd4a2e651412a6e4e9b6523c08fb7533124d440
Chirac <chiracmanoukian@hotmail.com>
2019-01-27 00:19:42 -0500

    Added migrations for remaining tables

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,11 +1,12 @@
-from __future__ import with_statement
 
+from __future__ import with_statement
+from alembic import context
+
+from sqlalchemy import pool
+from sqlalchemy import engine_from_config
+from models import Base
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
-from alembic import context
 import os
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -19,7 +20,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- backend/models.py
+++ backend/models.py
@@ -1,13 +1,62 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
 Base = declarative_base()
 
 
 class Testtable(Base):
     __tablename__ = 'testtable'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50))
+    id = Column('id', Integer, primary_key=True)
+    name = Column('name', String(50))
 
     def __init__(self, name):
         self.name = name
+
+
+class Songs(Base):
+    __tablename__ = 'songs'
+    tunes_id = Column(Integer, primary_key=True)
+    artist_id = Column(Integer, ForeignKey("artists.id"), nullable=False)
+    spotify_id = Column(Integer)
+    name = Column(String(50), nullable=False)
+    mode = Column(Integer)
+    acousticness = Column(Float)
+    danceability = Column(Float)
+    energy = Column(Float)
+    instrumentalness = Column(Float)
+    liveness = Column(Float)
+    loudness = Column(Float)
+    speechiness = Column(Float)
+    valence = Column(Float)
+    tempo = Column(Float)
+
+
+class Locations(Base):
+    __tablename__ = 'locations'
+    id = Column(Integer, primary_key=True)
+    longitude = Column(Float)
+    latitude = Column(Float)
+    mode = Column(Integer)
+    acousticness = Column(Float)
+    danceability = Column(Float)
+    energy = Column(Float)
+    instrumentalness = Column(Float)
+    liveness = Column(Float)
+    loudness = Column(Float)
+    speechiness = Column(Float)
+    valence = Column(Float)
+    tempo = Column(Float)
+
+
+class Plays(Base):
+    __tablename__ = 'plays'
+    id = Column(Integer, primary_key=True)
+    location_id = Column(
+        Integer, ForeignKey("locations.id"), nullable=False)
+    song_id = Column(Integer, ForeignKey("songs.tunes_id"), nullable=False)
+
+
+class Artists(Base):
+    __tablename__ = 'artists'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
