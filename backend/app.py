749ede411916a8bee71c9fa2aad7eed63feda85a
Chirac <chiracmanoukian@hotmail.com>
2019-01-27 08:19:41 -0500

    Fixed similarity calculator and finished endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, jsonify
-from models import Testtable
+from calculator import average_features, similarity
+from flask import Flask, jsonify, request
+from models import Testtable, Locations
+from connectors.SpotifyConnector import get_top_tracks, get_song_features
 from db import session
 from serializer import JSONSerializer
 import json
@@ -17,6 +19,21 @@ def hello():
     serialized = [json.dumps(r, cls=JSONSerializer) for r in res]
 
     return jsonify(result=serialized)
+
+
+@app.route('/api/myrecommendations/', methods=['GET'])
+def get_user_features():
+    user_token = request.headers.get('user_token')
+    top_track_ids = get_top_tracks(user_token)
+    track_features = get_song_features(top_track_ids)
+    res = session.query(Locations).all()
+    result_dict = [json.dumps(res, cls=JSONSerializer) for r in res]
+    similarity_scores = []
+    for location in result_dict:
+        similarity_scores.append({'score': similarity(track_features, location),
+                                  'longititude': location.longitude, 'latitude': location.latitude})
+
+    return jsonify(result=similarity_scores)
 
 
 if __name__ == '__main__':
